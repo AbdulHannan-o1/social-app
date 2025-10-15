2fb5c7b2535b2f5d3e9e10c3ea076bfe0e7562d2
AbdulHannan-o1 <shaikhhannan729@gmail.com>
2025-10-16 00:20:09 +0500

    url updated

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// mongoose.connect("mongodb://admin:admin123@localhost:27017/social-app?authSource=admin");
 if (mongoose.connection.readyState === 0) {
-  mongoose.connect(process.env.mongo_db_url)
+  mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("✅ MongoDB connected from user.js"))
     .catch(err => console.error("❌ MongoDB connection error:", err));
 }
+
 
 const userSchema = new mongoose.Schema({
     username: String,
