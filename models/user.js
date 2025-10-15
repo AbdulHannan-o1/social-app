ec826c1a8843b88e673dd3b34027e2cafdd07115
AbdulHannan-o1 <shaikhhannan729@gmail.com>
2025-10-15 22:53:43 +0500

    connecting to altas and vercel

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect("mongodb://admin:admin123@localhost:27017/social-app?authSource=admin");
+// mongoose.connect("mongodb://admin:admin123@localhost:27017/social-app?authSource=admin");
+mongoose.connect(process.env.mongo_db_url);
 
 const userSchema = new mongoose.Schema({
     username: String,
